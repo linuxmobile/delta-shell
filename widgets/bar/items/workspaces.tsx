0c221b88f1db574266fa9dd5bcb2d002608c9e64
sinomor <scarletweel@gmail.com>
2025-07-19 15:39:26 +0300

    refactor: use with component in bar for dynamic rendering

--- widgets/bar/items/workspaces.tsx
+++ widgets/bar/items/workspaces.tsx
@@ -1,17 +1,18 @@
 import { compositor } from "@/options";
 import { Workspaces_Niri } from "./workspaces_niri";
 import { Workspaces_Hypr } from "./workspaces_hypr";
+import { With } from "ags";
 
 export function Workspaces() {
    return (
       <box>
-         {compositor.get() === "niri" ? (
-            <Workspaces_Niri />
-         ) : compositor.get() === "hyprland" ? (
-            <Workspaces_Hypr />
-         ) : (
-            <box />
-         )}
+         <With value={compositor}>
+            {(comp) => {
+               if (comp === "niri") return <Workspaces_Niri />;
+               if (comp === "hyprland") return <Workspaces_Hypr />;
+               return <box />;
+            }}
+         </With>
       </box>
    );
 }
